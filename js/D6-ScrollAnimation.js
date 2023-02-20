f4434624662f1805902ed253e842f9d9e78e53cb
Annie Vargas <annie.vargas94@gmail.com>
2023-02-19 20:51:44 -0600

    D6 - Scroll Animation

--- js/D6-ScrollAnimation.js
+++ js/D6-ScrollAnimation.js
@@ -1,1 +1,19 @@
 console.log("Holis, si estoy conectado");
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
