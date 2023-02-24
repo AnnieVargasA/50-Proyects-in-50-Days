184c5e985aed922fe41f2c09b910e4c4a96956b6
Annie Vargas <annie.vargas94@gmail.com>
2023-02-24 00:01:18 -0600

    D7 - Split Landing Pages

--- js/D7-SplitLandingPage.js
+++ js/D7-SplitLandingPage.js
@@ -1,1 +1,19 @@
 console.log("Holis, si estoy conectado");
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () =>
+  container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+  container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+  container.classList.remove("hover-right")
+);
